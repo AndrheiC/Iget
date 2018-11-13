191b25c4697cbe59ea21ee47a61258a18d93c236
Andrhei <andrhei.acs@gmail.com>
2018-11-13 20:11:40 -0200

    Room para Compras feito

--- app/src/main/java/br/com/smarttech/acs/iget/database/RelCompraProdutoRoomDatabase.java
+++ app/src/main/java/br/com/smarttech/acs/iget/database/RelCompraProdutoRoomDatabase.java
@@ -9,9 +9,11 @@ import br.com.smarttech.acs.iget.DAO.CompraDAO;
 import br.com.smarttech.acs.iget.DAO.ProdutoDAO;
 import br.com.smarttech.acs.iget.DAO.RelCompraProdutoDAO;
 import br.com.smarttech.acs.iget.model.Compra;
+import br.com.smarttech.acs.iget.model.Pessoa;
 import br.com.smarttech.acs.iget.model.Produto;
+import br.com.smarttech.acs.iget.model.RelCompraProduto;
 
-@Database(entities = {Compra.class, Produto.class}, version = 1)
+@Database(entities = {Compra.class, Produto.class, Pessoa.class, RelCompraProduto.class}, version = 1)
 public abstract class RelCompraProdutoRoomDatabase extends RoomDatabase {
     private static volatile RelCompraProdutoRoomDatabase INSTANCE;
     public abstract RelCompraProdutoDAO relCompraProdutoDAO();
